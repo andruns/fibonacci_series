f6c648e5b81d6d7e8b279003212660e83f4c6404
andruns <and.runs1987@gmail.com>
2013-12-14 22:56:09 +0900

    write fibonacci module

--- lib/fibonacci_series.rb
+++ lib/fibonacci_series.rb
@@ -1,5 +1,16 @@
 require "fibonacci_series/version"
 
 module FibonacciSeries
-  # Your code goes here...
+  def at(num)
+    raise "invalid index" unless num > 0
+    if num == 1
+      0
+    elsif num == 2
+      1
+    else
+      at(num-1) + at(num-2)
+    end
+  end
+
+  module_function :at
 end
--- spec/fibonacci_series_spec.rb
+++ spec/fibonacci_series_spec.rb
@@ -1,11 +1,23 @@
 require 'spec_helper'
 
 describe FibonacciSeries do
-  it 'should have a version number' do
-    FibonacciSeries::VERSION.should_not be_nil
-  end
+  describe "#at" do
+    before do
+    end
 
-  it 'should do something useful' do
-    false.should be_true
+    it "is 0" do
+      FibonacciSeries.at(1).should == 0
+    end
+
+    it "is 8" do
+      FibonacciSeries.at(7).should == 8
+    end
+
+    it "is RuntimeError" do
+      lambda{FibonacciSeries.at(0)}.should raise_error(RuntimeError)
+    end
+
+    after do
+    end
   end
 end
